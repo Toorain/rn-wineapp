0bf713c4f660439ef179b8b2f65aa8acd4430968
WILMES <maxencewilmes@gmail.com>
2020-10-08 09:24:36 +0200

    v 0.0.2 Logo

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,6 +7,10 @@ import { Text, View } from '../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('../assets/images/winelogo.png')}
+      />
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.js" />
@@ -16,7 +20,7 @@ export default function TabOneScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -29,4 +33,9 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  logo: {
+    width: '30%',
+    height: '30%',
+    resizeMode: 'contain'
+  }
 });
